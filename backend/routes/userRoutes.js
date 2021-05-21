fcdec0b9a16db45940bc46686cc4a2acd8d8ebaa
Johannes <polyspark.prod@gmail.com>
2021-05-21 15:20:20 +0200

    added getUserById and getAllUsers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,8 @@ const userController = require("../controllers/userController");
 router.get('/whoami', userController.whoami)
 router.put("/:userId", userController.editUser); //Edit User
 router.post('/login', userController.loginUser)
+router.get('/:userId', userController.getUserById)
+router.get('', userController.getAllUsers)
 router.post('', userController.createUser)
 
 
