ac7a30af5dca06418034d2892864ca3aec30c099
daniel <daniel.palmdal@gmail.com>
2021-05-21 11:24:46 +0200

    Function loginUser() created
    Error handling added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,4 +3,9 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 
+
+
+
+router.post('/login', userController.loginUser)
+
 module.exports = router;
