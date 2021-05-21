35e27726b2a954031d0182ba9f4ec24cc911bcf7
Elena <sagva2014@gmail.com>
2021-05-21 19:36:29 +0200

    Added search by actor in getAllMovies

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -2,19 +2,20 @@ const Movie = require("../models/Movie");
 
 const getAllMovies = async (req, res) => {
 
-  //first we check if the query object has any content. If not we send back all users
+  //first we check if the query object has any content. If not we send back all movies
   if (Object.keys(req.query).length === 0) {
     let movies = await Movie.find().exec()
     res.json(movies)
     return
   }
 
-  //url for testing http://localhost:3001/api/v1/movies?title=cruella
-  let queryTitle = new RegExp(`^${req.query.title ? req.query.title : ''}\\w*`, 'gi') 
-  
+  //url for testing http://localhost:3001/api/v1/movies?actor=morgan&title=spiral
+  let queryTitle = new RegExp(`^${req.query.title ? req.query.title : ''}\\w*`, 'gi')
+  let queryActor = new RegExp(`${req.query.actor ? req.query.actor : ''}\\w*`, 'gi')
 
   //if the find method dosen't have any arguments, all documents will be return
-  let movies = await Movie.find({ Title: queryTitle}).exec()
+  // let movies = await Movie.find({ Title: queryTitle}).exec() 
+  let movies = await Movie.find({ Title: queryTitle, Actors: queryActor }).exec()
 
   if (movies.length === 0) {
     res.send('No movies matched the filter')
@@ -23,8 +24,7 @@ const getAllMovies = async (req, res) => {
 }
 
 
-const getMovieById = async (req, res) => { //for testing movie id 60a7866ff38ee6481eebbe35
-  console.log(`req.params`, req.params)
+const getMovieById = async (req, res) => { 
   Movie.findById(req.params.movieid).exec((err, movie) => {
     // Checks for thrown errors from the method itself.
     if (err) {
