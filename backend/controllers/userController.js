8135edc4c6544c3c6ecff467b0173fa9d8b2e9a2
daniel <daniel.palmdal@gmail.com>
2021-05-21 10:37:11 +0200

    function creteUser created
    also injected our testUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,17 @@
 const User = require("../models/User");
 const Encrypt = require("../Encrypt");
 
-module.exports = {};
+const createUser = async (req, res) => {
+
+    let user = await User.create(req.body);
+
+    console.log(req.body);
+    user.password = undefined;
+
+    res.json(user);
+
+
+}
+
+
+module.exports = {createUser};
