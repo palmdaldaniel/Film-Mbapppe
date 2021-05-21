eb69c184cc55e69ff70c259453267b39ad4d2461
Johannes <polyspark.prod@gmail.com>
2021-05-21 15:42:50 +0200

    indentation fix

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -2,18 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookingSchema = new Schema({
-    
-    userId:{type:Schema.Types.ObjectId, ref :"User", default: null},
-    showingId: {type:Schema.Types.ObjectId, ref :"Showings", default: null},
+
+    userId: { type: Schema.Types.ObjectId, ref: "User", default: null },
+    showingId: { type: Schema.Types.ObjectId, ref: "Showings", default: null },
     tickets: [
         {
-        ticketId: {type:Number},
-        price :{type:Number},
-        seatingNumber: {type:Number},
-    }
+            ticketId: { type: Number },
+            price: { type: Number },
+            seatingNumber: { type: Number },
+        }
     ]
 });
 
-const Booking  = mongoose.model("Booking", bookingSchema);
+const Booking = mongoose.model("Booking", bookingSchema);
 
 module.exports = Booking;
