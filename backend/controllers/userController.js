6b2c364832a929bf34c4a37de0d6178d26f60c0e
Johannes <polyspark.prod@gmail.com>
2021-05-21 14:20:29 +0200

    fixed indentation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,45 +20,45 @@ const createUser = async (req, res) => {
 
 // Edit User
 const editUser = async (req, res) => {
-    let user;
+  let user;
 
-    User.findById(req.params.userId).exec(async (err, result) => {
+  User.findById(req.params.userId).exec(async (err, result) => {
 
-      // checks for errors
-      if (err) {
-        res.status(400).json({ error: "Something went wrong" });
-        return;
-      }
-  
-      // in case no matching are found in the DB.
-      if (!result) {
-        res
-          .status(404)
-          .json({ error: `User with id ${req.params.userId} does not exist` });
-        return;
-      }
-  
-      user = result;
-      console.log("User: ", user);
-      Object.assign(user, req.body);
-      console.log("Updated user: ", user);
-  
-      // save it back in the DB .
-      await user.save();
-    });
-  
-    res.send("Ok");
-  }
+    // checks for errors
+    if (err) {
+      res.status(400).json({ error: "Something went wrong" });
+      return;
+    }
+
+    // in case no matching are found in the DB.
+    if (!result) {
+      res
+        .status(404)
+        .json({ error: `User with id ${req.params.userId} does not exist` });
+      return;
+    }
+
+    user = result;
+    console.log("User: ", user);
+    Object.assign(user, req.body);
+    console.log("Updated user: ", user);
+
+    // save it back in the DB .
+    await user.save();
+  });
+
+  res.send("Ok");
+}
 
 // log out
 const logout = (req, res) => {
-    console.log(
-      "Logged out:",
-      req.session.user.name,
-    );
-    delete req.session.user;
-    res.json({ success: "Logged out successfully" });
-  };
+  console.log(
+    "Logged out:",
+    req.session.user.name,
+  );
+  delete req.session.user;
+  res.json({ success: "Logged out successfully" });
+};
 
 
 module.exports = {
