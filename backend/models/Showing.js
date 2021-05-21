6b0743ebd2525e8fde6be5e76b453e5c878104e2
Johannes <polyspark.prod@gmail.com>
2021-05-21 11:40:12 +0200

    added: showings model

---
+++ backend/models/Showing.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const showingSchema = new Schema({
+  saloon: { type: Schema.Types.ObjectId, ref: "Saloon", required: true },
+  film: { type: Schema.Types.ObjectId, ref: "Movie", required: true },
+  date: [{ type: String, required: true }],
+  time: { type: String, required: true }
+});
+
+
+const Showing = mongoose.model("Showing", showingSchema);
+
+module.exports = Showing;
