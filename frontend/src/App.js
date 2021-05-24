1b8addb07df8caeb942d61b82db30aae00b086eb
Kyd <info@kydkit.com>
2021-05-24 15:16:01 +0200

    added ShowingPage.js to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import UserContextProvider from "./contexts/UserContext";
 import LoginPage from "./pages/LoginPage";
+import Showing from "./pages/ShowingPage"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <BrowserRouter>
           <h1>Mbappe</h1>
           <LoginPage />
+          <Route exact path="/showing" component={Showing} />
         </BrowserRouter>
       </UserContextProvider>
     </div>
