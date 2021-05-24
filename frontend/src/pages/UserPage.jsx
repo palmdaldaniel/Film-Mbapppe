9eea9df252a301e94e7c4f2bdb63247012cc7737
Johannes <polyspark.prod@gmail.com>
2021-05-24 15:43:05 +0200

    simple layout

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -4,24 +4,19 @@ const UserPage = () => {
   return (
     <div className={styles.container}>
       <h1>Userpage</h1>
-      <div className={styles.content}>
+      <div className={styles.userInfo}>
         <div className={styles.info}>
-          <h2>Welcome <span>Johannes</span></h2>
+          <h2>Hi, <span>Johannes</span>!</h2>
           <button className={styles.editNameButton}>Edit name</button>
-        </div>
-        <div className={styles.logout}>
-          <button className={styles.logoutButton}>
-            Logout
-          </button>
         </div>
       </div>
 
       <div className={styles.showings}>
-        <div className={styles.upcomming}>
+        <div className={styles.upcoming}>
           <h2>Upcoming movies</h2>
         </div>
         <div className={styles.previous}>
-          <h2>Prevous movies</h2>
+          <h2>Previous movies</h2>
         </div>
       </div>
 
