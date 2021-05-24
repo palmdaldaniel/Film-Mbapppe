39003807eec721f5ab738206fba7d7da541df9c8
Johannes <polyspark.prod@gmail.com>
2021-05-24 14:41:34 +0200

    added markup for userpage

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -2,11 +2,23 @@ const UserPage = () => {
   return (
     <div className="container">
       <h1>Userpage</h1>
-      <h2>Upcoming movies</h2>
-      <h2>Prevous movies</h2>
-      
+      <div className="info">
+        <h3>Name: <span>Johannes</span></h3>
+        <h3>Email: <span>Joh@mail.com</span></h3>
+        <button>Edit name</button>
+        <button>Logout</button>
+      </div>
+
+      <div className="upcoming">
+        <h2>Upcoming movies</h2>
+      </div>
+
+      <div className="previous">
+        <h2>Prevous movies</h2>
+      </div>
+
     </div>
   );
 }
- 
+
 export default UserPage;
