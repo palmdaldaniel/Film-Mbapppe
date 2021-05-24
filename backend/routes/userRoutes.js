64b6c8e332fea4d7d244e89c633222d56ff0d52e
Johannes <polyspark.prod@gmail.com>
2021-05-24 09:58:42 +0200

    fixed: userRoutes "logout url

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,7 @@ const userController = require("../controllers/userController");
 router.get('/whoami', userController.whoami)
 router.put("/:userId", userController.editUser) //Edit User
 router.post('/login', userController.loginUser)
-router.post('/login', userController.logout) //log out user
+router.post('/logout', userController.logout) //log out user
 router.get('/:userId', userController.getUserById)
 router.get('', userController.getAllUsers)
 router.post('', userController.createUser)
