19461d60f0dc2c2e37f352d5de3f066802533760
Johannes <polyspark.prod@gmail.com>
2021-05-25 10:24:15 +0200

    fixed userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,80 @@
 const User = require("../models/User");
 const Encrypt = require("../Encrypt");
-const utils = require("../core/utilities");
 
+const getAllUsers = async (req, res) => {
+  let users = await User.find().exec();
+  res.json(users);
+};
+
+const getUserById = async (req, res) => {
+  // with callback and exec()
+  User.findById(req.params.userId).exec((err, user) => {
+    if (err) {
+      res.status(400).json({ error: "Something went wrong" });
+      return;
+    }
+
+    if (!user) {
+      res
+        .status(404)
+        .json({ error: `User with id ${req.params.userId} does not exist` });
+      return;
+    }
+
+    user.password = undefined;
+
+    res.json(user);
+  });
+};
+
+const createUser = async (req, res) => {
+  // destructure req.body object
+  const { email } = req.body;
+
+  let userExists = await User.exists({ email: email });
+
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ error: "An user with that email already exists" });
+  }
+
+  let user = await User.create(req.body);
+  user.password = undefined;
+  res.json(user);
+};
+
+// Edit User
+const editUser = async (req, res) => {
+  let user;
+
+  User.findById(req.params.userId).exec(async (err, result) => {
+
+    // checks for errors
+    if (err) {
+      res.status(400).json({ error: "Something went wrong" });
+      return;
+    }
+
+    // in case no matching are found in the DB.
+    if (!result) {
+      res
+        .status(404)
+        .json({ error: `User with id ${req.params.userId} does not exist` });
+      return;
+    }
+
+    user = result;
+    console.log("User: ", user);
+    Object.assign(user, req.body);
+    console.log("Updated user: ", user);
+
+    // save it back in the DB .
+    await user.save();
+  });
+
+  res.send("Ok");
+}
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
@@ -27,78 +100,31 @@ const loginUser = async (req, res) => {
     return res.status(401).json({ error: "Wrong email or password" });
   }
   return res.status(401).json({ error: "Wrong email or password" });
-};
 
-const createUser = async (req, res) => {
-  // destructure req.body object
-  const { email, password } = req.body;
-
-  // check if password is strong.
-  const passwordIsStrong = utils.passwordValidator(password);
-
-  // check if email is valid.
-  const validemail = utils.emailValidator(email);
-
-  if (!passwordIsStrong) {
-    return res.status(400).json({ err: "Put in a stronger password " });
-  } else if (!validemail) {
-    return res.status(400).json({ err: "Put in a valid email" });
-  }
-
-  // check if user already exists with that email
-  let userExists = await User.exists({ email: email });
-
-  if (userExists) {
-    return res
-      .status(400)
-      .json({ error: "An user with that email already exists" });
-  }
-  let user = await User.create(req.body);
-  user.password = undefined;
-  res.json(user);
-};
-
-// Edit User
-const editUser = async (req, res) => {
-  let user;
-
-  User.findById(req.params.userId).exec(async (err, result) => {
-    // checks for errors
-    if (err) {
-      res.status(400).json({ error: "Something went wrong" });
-      return;
-    }
-
-    // in case no matching are found in the DB.
-    if (!result) {
-      res
-        .status(404)
-        .json({ error: `User with id ${req.params.userId} does not exist` });
-      return;
-    }
-
-    user = result;
-    console.log("User: ", user);
-    Object.assign(user, req.body);
-    console.log("Updated user: ", user);
-
-    // save it back in the DB .
-    await user.save();
-  });
-
-  res.send("Ok");
 };
 
 // log out
 const logout = (req, res) => {
-  console.log("Logged out:", req.session.user.name);
+  console.log(
+    "Logged out:",
+    req.session.user.name,
+  );
   delete req.session.user;
   res.json({ success: "Logged out successfully" });
 };
+
+const whoami = async (req, res) => {
+  // in postman res.json will say null if no user is logged in
+  return res.json(req.session.user || null);
+}
 
 module.exports = {
   logout,
   editUser,
   createUser,
   loginUser,
+  whoami,
+  getAllUsers,
+  getUserById
 };
+
