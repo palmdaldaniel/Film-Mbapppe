aa00a5a9abb0f95322639a4e38be9a2bae16cd4c
Jakob <jsandberg_8@hotmail.com>
2021-05-25 10:55:17 +0200

    Change in the LoginForm

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -13,52 +13,50 @@ export default function Login() {
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
-      };
-    
-      const handlePasswordChange = (e) => {
+    };
+
+    const handlePasswordChange = (e) => {
         setPassword(e.target.value);
-      };
+    };
 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         let loginInfo = {
-          email,
-          password,
+            email,
+            password,
         };
         let result = await loginUser(loginInfo);
         if (result.success) {
-          console.log(result.success);
-          history.push("/");
+            console.log(result.success);
+            history.push("/");
         } else {
-          setError(result.error);
+            setError(result.error);
         }
-      };
-    
+    };
 
-    
     return (
         <div className={styles.container}>
             <p className={styles.loginformtext}>Welcome back. Log in to see your movie points and more!</p>
             <div className={styles.loginform}>
-            <Form
-              onSubmit={(e) => {
-                handleSubmit(e);
-              }}
-            >
-                <Alert variant={"danger"} className={`${styles.errorBox} ${error ? styles.active : styles.inactive}`}>You did not enter the correct credentials</Alert>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Control className={styles.inputField} onChange={handleEmailChange} size="lg" type="email" placeholder="Enter email" required />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Control className={styles.inputField} onChange={handlePasswordChange} size="lg" type="password" placeholder="Password" required />
-                </Form.Group>
-                <Container className="text-center">
-                    <Button className={styles.singInButton} variant="danger" type="submit">
-                        SIGN IN
+                <Form
+                    onSubmit={(e) => {
+                        handleSubmit(e);
+                    }}
+                >
+                    <Alert variant={"danger"} className={`${styles.errorBox} ${error ? styles.active : styles.inactive}`}>You did not enter the correct credentials</Alert>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control className={styles.inputField} onChange={handleEmailChange} size="lg" type="email" placeholder="Enter email" required />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control className={styles.inputField} onChange={handlePasswordChange} size="lg" type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <Container className="text-center">
+                        <Button className={styles.singInButton} variant="danger" type="submit">
+                            SIGN IN
                     </Button>
-                </Container>
-            </Form>
+                    </Container>
+                </Form>
             </div>
         </div>
 
