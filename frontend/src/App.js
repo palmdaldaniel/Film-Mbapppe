e4c1dd7492b46aa6384f6d542d5d8cc4a13cfe78
Johannes <polyspark.prod@gmail.com>
2021-05-25 10:32:25 +0200

    added user profile to aspp.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import UserContextProvider from './contexts/UserContext';
 //import LoginPage from './pages/LoginPage';
 import UserPage from './pages/UserPage';
 import { BrowserRouter, Route } from "react-router-dom";
-import UserContextProvider from "./contexts/UserContext";
 import LoginPage from "./pages/LoginPage";
 import Showing from "./pages/ShowingPage";
 
@@ -19,6 +18,9 @@ function App() {
           {/* add /:showingid for showing page */}
           <Route exact path="/showing">
             <Showing />
+          </Route>
+          <Route exact path="/profile">
+            <UserPage />
           </Route>
         </BrowserRouter>
       </UserContextProvider>
