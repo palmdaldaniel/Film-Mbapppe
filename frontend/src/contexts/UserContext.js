8d080bf1a06a8a51d9473016ccc40323ef2eff59
Johannes <polyspark.prod@gmail.com>
2021-05-25 15:21:20 +0200

    pulled dev into this

--- frontend/src/contexts/UserContext.js
+++ frontend/src/contexts/UserContext.js
@@ -14,7 +14,7 @@ const UserContextProvider = (props) => {
   const getUser = async () => {
     //let user = await fetch("/api/v1/users/whoami");
     //user = await user.json();
-    let user = { name: "Johannes", email: "bob@mail.com" };
+    let user = { name: "Bob", email: "bob@mail.com" }; //delete after testing
     setActiveUser(user)
     return
   }
