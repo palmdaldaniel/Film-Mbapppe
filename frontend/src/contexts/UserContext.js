fff1803983dff69550d0334c47ea151a15d8158d
Blerta <blertatoska30@gmail.com>
2021-05-26 09:28:13 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- frontend/src/contexts/UserContext.js
+++ frontend/src/contexts/UserContext.js
@@ -3,8 +3,11 @@ import { createContext, useState } from "react"
 export const UserContext = createContext();
 
 const UserContextProvider = (props) => {
+  const history = useHistory();
   const [activeUser, setActiveUser] = useState(null);
   const [bookings, setBookings] = useState([]);
+  const [feedbackRegistration, setFeedbackRegistration] = useState(null);
+  const [userToRegister, setUserToRegister] = useState([]);
 
 
    //useEffect(() => {
@@ -44,13 +47,38 @@ const createUser = async(newUser)=>{
   return result;
 }
 
+
 const logout = async ()=>{
   await fetch("/api/v1/users/logout")
    getUser()
 }
 
+// whoami
+const whoami = async () => {
+  let userlogin = await fetch("/api/v1/users/whoami");
+  userlogin = await userlogin.json();
+  if (userlogin) {
+    setUser(userlogin);
+  };
 
 
+//register user 
+const register = async (userToRegister) => {
+  let userToAdd = await fetch('/api/v1/users/register', {
+    method: 'POST',
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(userToRegister)
+  });
+
+  userToAdd = await userToAdd.json();
+
+  if (userToAdd.success) {
+    console.log(userToAdd.success)
+  } else if (userToAdd.error) {
+    console.log(userToAdd.error)
+    setCurrentUser(undefined);
+  }
+};
 
   
   const values =
@@ -62,13 +90,20 @@ const logout = async ()=>{
     loginUser,
     createUser,
     logout,
-    getUser
+    getUser,
+    whoami,
+    userlogin,
+    register,
+    
+    setFeedbackRegistration,
+    feedbackRegistration,
 
   }
   return (
     <UserContext.Provider value={values}>
       {props.children}
     </UserContext.Provider>
-  )
-}
-export default UserContextProvider
+  );
+  }
+};
+export default UserContextProvider;
