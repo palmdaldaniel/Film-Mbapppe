c0ac4f8c18692e02c5f508a476f2ed2c65740792
Elena <sagva2014@gmail.com>
2021-05-26 13:53:59 +0200

    Added styling for pagination

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -1,22 +1,21 @@
 import Pagination from 'react-bootstrap/Pagination';
-
 const PaginationComponent = (props) => {
-    const {activPage, pageTotal, setCurrentPage} = props.values
+    const { activPage, pageTotal, setCurrentPage } = props.values
     let active = activPage; //active page which is highlighted
     let items = [];//how many pages in the pagination bar
 
     for (let number = 1; number <= pageTotal; number++) {
         items.push(
-            <Pagination.Item key={number} active={number === active} onClick={()=>handleClick(number)}>
+            <Pagination.Item key={number} active={number === active} onClick={() => handleClick(number)}>
                 {number}
             </Pagination.Item>,
         );
     }
 
-    const handleClick =(number)=> {
+    const handleClick = (number) => {
         setCurrentPage(number)
     }
-    
+
     const paginationBasic = (
         <div>
             <Pagination className='justify-content-center mt-3'>{items}</Pagination>
