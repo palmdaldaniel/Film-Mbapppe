474d17246ac7911290c22b2f44863ea508b6dfcd
Kyd <info@kydkit.com>
2021-05-26 16:00:59 +0200

    make in put field and retrieve info

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,9 +1,27 @@
+import { useEffect, useContext } from "react";
+import { MovieContext } from "../contexts/MovieContext";
+import styles from "../css/Search.module.css";
+
 const Search = () => {
+  const { getAllMovies } = useContext(MovieContext);
+  useEffect(() => {
+    getAllMovies();
+  }, []);
+
+  const handleSearch = (e) => {
+    console.log(e.target.value);
+  }
+
   return (
     <div>
-      <h1>Search feature here</h1>
+      <input
+        className={styles.searchInput}
+        type="text"
+        placeholder="Search for movies or actors"
+        onChange={(e) => handleSearch(e)}
+      />
     </div>
   );
-}
- 
+};
+
 export default Search;
