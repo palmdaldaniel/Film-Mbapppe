638213afdcf973b4263aa5da38f275214c9e4802
Johannes <polyspark.prod@gmail.com>
2021-05-26 11:00:41 +0200

    commented out userContext

--- frontend/src/contexts/UserContext.js
+++ frontend/src/contexts/UserContext.js
@@ -1,30 +1,30 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 
 export const UserContext = createContext();
 
 const UserContextProvider = (props) => {
-  const history = useHistory();
+  //const history = useHistory();
   const [activeUser, setActiveUser] = useState(null);
   const [bookings, setBookings] = useState([]);
   const [loginResult, setLoginResult] = useState(null);
 
   // toggle between loginForm and register in LoginPage
-  const [showLogin, setShowLogin] = useState(true);
+  //const [showLogin, setShowLogin] = useState(true);
 
 
-   useEffect(() => {
+  useEffect(() => {
     getUser();
-  }, []) 
+  }, [])
 
   const getUser = async () => {
     let user = await fetch("/api/v1/users/whoami")
     user = await user.json();
     //setUser(user)
     return
-  } 
+  }
 
-  const loginUser = async (loginInfo)=>{
-    let userLoggingIn = await fetch("/api/v1/users/login",{
+  const loginUser = async (loginInfo) => {
+    let userLoggingIn = await fetch("/api/v1/users/login", {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -43,77 +43,77 @@ const UserContextProvider = (props) => {
 
 
 
-    
+
     return userLoggingIn;
-}
-
-
-
-
-const createUser = async(newUser)=>{
-  let result = await fetch("/api/v1/users", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(newUser),
-  });
-  result = await result.json();
-  getUser();
-  return result;
-}
-
-
-const logout = async ()=>{
-  await fetch("/api/v1/users/logout")
-   getUser()
-}
-
-// whoami
-const whoami = async () => {
-  let userlogin = await fetch("/api/v1/users/whoami");
-  userlogin = await userlogin.json();
-  if (userlogin) {
-    setUser(userlogin);
-  };
-
-
-//register user 
-const register = async (userToRegister) => {
-  let userToAdd = await fetch('/api/v1/users/register', {
-    method: 'POST',
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify(userToRegister)
-  });
-
-  userToAdd = await userToAdd.json();
-
-  if (userToAdd.success) {
-    console.log(userToAdd.success)
-  } else if (userToAdd.error) {
-    console.log(userToAdd.error)
-    setCurrentUser(undefined);
   }
-};
 
-  
-  const values =
-  {
-    activeUser, 
-    setActiveUser, 
-    bookings,
-    setBookings,
-    loginUser,
-    createUser,
-    logout,
-    getUser
 
+
+
+  const createUser = async (newUser) => {
+    let result = await fetch("/api/v1/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    });
+    result = await result.json();
+    getUser();
+    return result;
   }
-  return (
-    <UserContext.Provider value={values}>
-      {props.children}
-    </UserContext.Provider>
-  );
+
+
+  const logout = async () => {
+    await fetch("/api/v1/users/logout")
+    getUser()
+  }
+
+  // whoami
+  const whoami = async () => {
+    let userlogin = await fetch("/api/v1/users/whoami");
+    userlogin = await userlogin.json();
+    if (userlogin) {
+      //setUser(userlogin);
+    };
+
+
+    //register user 
+    const register = async (userToRegister) => {
+      let userToAdd = await fetch('/api/v1/users/register', {
+        method: 'POST',
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(userToRegister)
+      });
+
+      userToAdd = await userToAdd.json();
+
+      if (userToAdd.success) {
+        console.log(userToAdd.success)
+      } else if (userToAdd.error) {
+        console.log(userToAdd.error)
+        //setCurrentUser(undefined);
+      }
+    };
+
+
+    const values =
+    {
+      activeUser,
+      setActiveUser,
+      bookings,
+      setBookings,
+      loginUser,
+      createUser,
+      logout,
+      getUser
+
+    }
+    return (
+      <UserContext.Provider value={values}>
+        {props.children}
+      </UserContext.Provider>
+    );
   }
 };
 export default UserContextProvider;
