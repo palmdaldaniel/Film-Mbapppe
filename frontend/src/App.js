25da01426c1d168fbe92309e9dccf9ed168a6c1c
Johannes <polyspark.prod@gmail.com>
2021-05-26 11:34:28 +0200

    indentation fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,29 +13,29 @@ function App() {
   return (
     <div className="App">
       <MovieContextProvider>
-      <UserContextProvider>
-        <BrowserRouter>
-        <Navbar />
-          <h1>Mbappe</h1>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route exact path="/login">
-            <LoginPage />
-          </Route>
+        <UserContextProvider>
+          <BrowserRouter>
+            <Navbar />
+            <h1>Mbappe</h1>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route exact path="/login">
+              <LoginPage />
+            </Route>
 
-          <Route exact path="/showing/:showingId" component={Showing}/>
+            <Route exact path="/showing/:showingId" component={Showing} />
 
-          {/* add /:showingid for showing page */}
-          <Route exact path="/allmovies"  />
-              <Route exact path="/about" />
-              <Route exact path="/profile"  />
-              <Route exact path="/login"  />
-          <Route exact path="/allmovies">
-            <AllMovies />
-          </Route>
-        </BrowserRouter>
-      </UserContextProvider>
+            {/* add /:showingid for showing page */}
+            <Route exact path="/allmovies" />
+            <Route exact path="/about" />
+            <Route exact path="/profile" />
+            <Route exact path="/login" />
+            <Route exact path="/allmovies">
+              <AllMovies />
+            </Route>
+          </BrowserRouter>
+        </UserContextProvider>
       </MovieContextProvider>
     </div>
   );
