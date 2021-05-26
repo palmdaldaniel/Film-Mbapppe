424c9cceda88e88ed102bbb9a3a3b4174a864e89
Johannes <polyspark.prod@gmail.com>
2021-05-26 11:22:56 +0200

    homepage not found in dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@
 import UserPage from './pages/UserPage';
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
 import AllMovies from "./pages/AllMovies";
 import UserContextProvider from './contexts/UserContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
