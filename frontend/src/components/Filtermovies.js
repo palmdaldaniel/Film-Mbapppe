591e3f3d31b9581b948a39fa401076803d43eea6
daniel <daniel.palmdal@gmail.com>
2021-05-27 13:30:09 +0200

    values for labels director and language created

--- frontend/src/components/Filtermovies.js
+++ frontend/src/components/Filtermovies.js
@@ -8,12 +8,14 @@ const Filtermovies = ({ movies }) => {
   const [years, setYears] = useState([]);
   const [ratings, setRatings] = useState([]);
   const [runtimes, setRuntimes] = useState([]);
-  
-  useEffect(() => {
+  const [directors, setDirectors] = useState([]);
+  const [languages, setLanguages] = useState([]);
+  console.log(languages);
 
+  useEffect(() => {
     // fire helperfunctions on pageload
     makeLabels(movies);
-  
+
     makeValues(movies);
   }, []);
 
@@ -27,34 +29,32 @@ const Filtermovies = ({ movies }) => {
     setLabels(labels);
   };
 
- /*  const makeValuesForGenres = (movies) => {
+  /*  const makeValuesForGenres = (movies) => {
     const values = movies.map((value) => value.Genre).flat();
     const unique = [...new Set(values)];
     setGenres(unique);
   }; */
 
   const makeValues = (movies) => {
-    console.log('movies', movies);
-    
     // create values for label genre
-    setGenres([...new Set(movies.map((value) => value.Genre).flat())])
+    setGenres([...new Set(movies.map((value) => value.Genre).flat())]);
 
-    //create values for label year 
-   const years = movies.map(value => value.Year).sort((a,b) => a - b)    
-    setYears([...new Set(years)])
+    //create values for label year
+    const years = movies.map((value) => value.Year).sort((a, b) => a - b);
+    setYears([...new Set(years)]);
 
     // create values for label rating
-    setRatings([...new Set(movies.map(value => value.Rated))])
+    setRatings([...new Set(movies.map((value) => value.Rated))]);
 
-    // create value for runtime  - t
-    setRuntimes([...new Set(movies.map(value => value.Runtime))])
-  
-    
+    // create values for runtime
+    setRuntimes([...new Set(movies.map((value) => value.Runtime))]);
 
-  }
+    // create values for Director
+    setDirectors([...new Set(movies.map((value) => value.Director))]);
 
-  
-
+    // create values for Language
+    setLanguages([...new Set(movies.map((value) => value.Language))]);
+  };
 
   return (
     <div className="FilterContainer">
