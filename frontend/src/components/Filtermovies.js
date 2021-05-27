1cc182f590b1619ce9b9b49cfd84118a1495dc1e
daniel <daniel.palmdal@gmail.com>
2021-05-27 12:13:28 +0200

    Refactor code for label
    Loops out only the values atm

--- frontend/src/components/Filtermovies.js
+++ frontend/src/components/Filtermovies.js
@@ -2,49 +2,39 @@ import { useEffect, useState } from "react";
 
 const Filtermovies = ({ movies }) => {
   const [labels, setLabels] = useState([]);
-  const [genres, setGenres] = useState([])
- 
+  const [genres, setGenres] = useState([]);
 
   useEffect(() => {
     makeLabels(movies);
-    makeValuesForGenre(movies)
+    makeValuesForGenre(movies);
   }, []);
 
-  // create labels from the movie object keys
+  // create labels from the movie object keys, might not be nessary
   const makeLabels = (movies) => {
     let labels = Object.keys(movies[0]);
     let values = ["_id", "Poster", "Trailer", "Plot", "Actors", "Title", "__v"];
 
-    // take out the labels we don't want to filter on. Will use it to create select fields on
+    // take out the labels we don't want to filter on. Will use it to create select fields in return statement
     labels = labels.filter((label) => !values.includes(label));
     setLabels(labels);
   };
 
   const makeValuesForGenre = (movies) => {
-    const values = movies.map(value => value.Genre).flat();
-    let unique = [...new Set(values)]
+    const values = movies.map((value) => value.Genre).flat();
+    let unique = [...new Set(values)];
     setGenres(unique);
-}
-  
-
-
-
-
+  };
 
   return (
     <div className="FilterContainer">
-      <h1 style={{ color: "white" }}> filter filter </h1>
-      {labels.map((label, i) => {
-        return (
-          <select name={label} value={label} required key={i}>
-            <option value={label}> {label}</option>
-
-        
-
-
-          </select>
-        );
-      })}
+      <select name="Genre" required>
+        <option value=""> Genre </option>
+        {genres.map((genre) => (
+          <option value={genre} key={genre}>
+            {genre}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
