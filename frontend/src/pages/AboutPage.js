5e8a989a3b4ee6dbc90c69f06ada07f2f6b14178
Blerta <blertatoska30@gmail.com>
2021-06-01 13:11:31 +0200

    Fixed style in About

---
+++ frontend/src/pages/AboutPage.js
@@ -0,0 +1,68 @@
+import styles from "../css/AboutPage.module.css";
+import "../css/AboutPage.module.css";
+//import chair from "/Users/blerta/Film-Mbapppe/frontend/src/red.jpeg";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <div className={styles.about2}>
+        
+      </div>
+      <div className={styles.aboutContainer}>
+        <div className={styles.about}>
+          <h1>Filmvissarna</h1>
+          <h3>About us</h3>
+          <p>
+            {" "}
+            Filmvissarnase is the oldest and first website dedicated exclusively
+            to Malmö Cinema, and has been the most trusted cinema guide on the
+            web for the past 22 years. Founded i n early 1995 at the dawn of
+            commercial internet use, the website continues to be one of the
+            world's highest ranked theatre websites and most trusted sources for
+            Malmö theatre listings, news and reviews.
+          </p>
+          <br />
+
+          <p>
+            Founded by the original site Editor Darren Dalglish in 1995,
+            LondonTheatre.co.uk was born from the growing need for UK and
+            international fans of London theatre to read news, reviews and
+            features. Growing quickly through the 90s, along with its first
+            dedicated weekly theatre newsletter, the website built loyal
+            visitors from around the world who would use the site to plan trips
+            to the West End and beyond..
+          </p>
+
+          <p>
+            Founded by the original site Editor Darren Dalglish in 1995,
+            LondonTheatre.co.uk was born from the growing need for UK and
+            international fans of London theatre to read news, reviews and
+            features. Growing quickly through the 90s, along with its first
+            dedicated weekly theatre newsletter, the website built loyal
+            visitors from around the world who would use the site to plan trips
+            to the West End and beyond..
+          </p>
+        </div>
+
+        <div className={styles.about1}>
+          <div className="öppet">
+            <h4>Address</h4>
+            <p>Gustav Torg 1</p>
+            <p>Malmö, Sweden</p>
+            <br></br>
+          </div>
+
+          <div className="contactContainer">
+            <div className="contact">
+              <h4>Contact</h4>
+              <p>filmvissarna@gmx.com</p>
+              <p>070 98 76 12</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
