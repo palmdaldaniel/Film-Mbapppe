6e3b554d3bcb35fdda44ecf0ba1e4f60da88b4d7
Kyd <kyd.kitchaiya@medieinstitutet.se>
2021-06-01 22:20:08 +0200

    Msfixed double scrolling. styled movie info page

--- frontend/src/components/MovieInfo.js
+++ frontend/src/components/MovieInfo.js
@@ -5,9 +5,9 @@ const MovieInfo = (props) => {
 
   let film = null
 
-  if (props && props.showing ) {
+  if (props && props.showing) {
     film = props.showing.film
-  } else if (props ){
+  } else if (props) {
     film = props.movie
   }
 
@@ -17,23 +17,29 @@ const MovieInfo = (props) => {
   if (film) {
     content = (
       <div className={styles.movieinfocontainer}>
-        <div className={styles.postercontainer}>
-          <strong className={styles.title}>{film.Title}</strong>
-          <img className={styles.poster} src={film.Poster} alt="movie poster" />
-        </div>
-        <div className={styles.descContainer}>
-          <p><span className={styles.bold}>Runtime:</span>  {film.Runtime}</p>
-          <p><span className={styles.bold}>Genre:</span> {film.Genre}</p>
-          <p><span className={styles.bold}>Age Rating:</span> {film.Rated}</p>
-          <p><span className={styles.bold}>Language:</span> {film.Language}</p>
-          <p><span className={styles.bold}>Starring:</span>
-            {film.Actors[0]}, {film.Actors[1]}
-          </p>
-          <p><span className={styles.bold}>Director:</span> {film.Director}</p>
-          <p><span className={styles.bold}>Description:</span> {film.Plot}</p>
-        </div>
-        <div className={styles.iframe}>
-          <Video video={film.Trailer} />
+        <strong className={styles.title}>{film.Title}</strong>
+
+        <div className={styles.miniwrapper}>
+
+          <div className={styles.posterAndText}>
+            <img className={styles.poster} src={film.Poster} alt="movie poster" />
+            <div className={styles.descContainer}>
+              <p><span className={styles.bold}>Runtime:</span>  {film.Runtime}</p>
+              <p><span className={styles.bold}>Genre:</span> {film.Genre}</p>
+              <p><span className={styles.bold}>Age Rating:</span> {film.Rated}</p>
+              <p><span className={styles.bold}>Language:</span> {film.Language}</p>
+              <p><span className={styles.bold}>Starring:</span>
+                {film.Actors[0]}, {film.Actors[1]}
+              </p>
+              <p><span className={styles.bold}>Director:</span> {film.Director}</p>
+              <p><span className={styles.bold}>Description:</span> {film.Plot}</p>
+            </div>
+          </div>
+
+          <div className={styles.iframe}>
+            <Video video={film.Trailer} />
+          </div>
+          
         </div>
       </div>
     );
