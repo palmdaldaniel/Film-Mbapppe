1c23c9766b46ed6d91ed0b8e18fe0115f9f2a9f8
Blerta <blertatoska30@gmail.com>
2021-06-01 23:13:30 +0200

    made signUp responsive

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -66,7 +66,7 @@ const Register = () => {
 
   return (
     <div className={styles.registercontainer}>
-      <h1 className={styles.header}>MOVIEPLUS</h1>
+      <h1 className={styles.header}>Filmvisarna</h1>
       <Container className={`${styles.containerStyle} py-0`}>
         {signUpDone ? (
           <div className="confirmationDiv">
@@ -119,7 +119,7 @@ const Register = () => {
                       <p className="error">This email is already at use.</p>
                     )}
                   </Form.Group>
-
+                  <div className={styles.regarea}>
                   <Button
                     className={styles.registerButton}
                     variant="danger"
@@ -128,6 +128,7 @@ const Register = () => {
                   >
                     Register
                   </Button>
+                  </div>
                 </form>
               </Form>
               {}{" "}
