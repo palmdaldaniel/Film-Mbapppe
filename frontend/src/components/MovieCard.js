3d9080242cdc511468eb029e3723994ac9007134
Jakob <jsandberg_8@hotmail.com>
2021-06-02 10:29:53 +0200

    Hover effect on moviecard

--- frontend/src/components/MovieCard.js
+++ frontend/src/components/MovieCard.js
@@ -1,24 +1,45 @@
 import Card from 'react-bootstrap/Card';
 import styles from "../css/MovieCard.Module.css";
+import { useHistory } from "react-router-dom";
     
 
 function MovieCard(props) {
+    const history = useHistory();
     const { movie } = props;
 
+    const handleClick = () => {
+        history.push(`/movie-info/${movie._id}`);
+    };
+
     return (
-        
-           <Card className={`mx-1 mx-md-3`} style={{width: '270px', background: 'none'}} >
-                <Card.Img variant="top" src={movie.Poster} style={{ height: '390px' }} />
+            <div className={styles.hoverZoom} >
+           <Card className={`mx-1 mx-md-3`} style={{width: '270px', background: 'none'}} onClick={handleClick}>
+
+               <div className={styles.item}>
+                   <span className={styles.first}>
+                       <Card.Img variant="top" src={movie.Poster} style={{ height: '390px' }} />
+                       </span>
+
+                   <span className={styles.second}>
+                       <Card.Img className={styles.imgAfter} variant="top" src={movie.Poster} />
+                       <p className={styles.textAfter}>
+                           
+                           Director: {movie.Director}
+                           </p>
+                       </span>
+                
+                </div >
+
                 <Card.Body>
                     <Card.Text className={`${styles.cardTitle} text-center mb-1`}>{movie.Title}</Card.Text>
                     <Card.Text className={`${styles.cardText} text-center`}>
-                        <span>{movie.Genre}</span>   
+                        <span>{movie.Genre[0]}</span>   
                         <br/>
                         <span>{movie.Actors[0]}</span> 
                     </Card.Text>
                 </Card.Body>
             </Card>
-        
+        </div>
     )
 }
 
