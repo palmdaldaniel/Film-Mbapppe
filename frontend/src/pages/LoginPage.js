a748a94e2fa115d6465464b899cec4712d3a66cf
Jakob <jsandberg_8@hotmail.com>
2021-06-02 11:44:09 +0200

    Small fix

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -3,11 +3,9 @@ import LoginForm from "../components/LoginForm";
 import { Container } from "react-bootstrap"
 import { UserContext } from "../contexts/UserContext"
 import styles from "../css/login.module.css"
-import { useHistory } from "react-router-dom";
 import SignUp from "../components/SignUp";
 
 export default function LoginPage() {
-  const history = useHistory();
   const { showLogin, setShowLogin } = useContext(UserContext)
   const toggle = () => {
     setShowLogin(!showLogin)
@@ -17,7 +15,6 @@ export default function LoginPage() {
     <div className={styles.logincontainer}>
     <Container className={`${styles.containerStyle} py-0`} >
       {showLogin ? <LoginForm /> : <SignUp />}
-      
       <p className={styles.toggleText} onClick={toggle}>{showLogin ? "Are you not a member yet? " : "Back to login"}</p>
     </Container>
   </div>
