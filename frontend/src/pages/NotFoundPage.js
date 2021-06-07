843faff4d468767002bb2e286a7edd6a99eea2c0
daniel <daniel.palmdal@gmail.com>
2021-06-07 08:24:12 +0200

    Link to loginpage added

--- frontend/src/pages/NotFoundPage.js
+++ frontend/src/pages/NotFoundPage.js
@@ -1,7 +1,10 @@
+import {Link }  from 'react-router-dom'
+
 const NotFoundPage= () => {
     return (  
         <div style={{height: '50vh', textAlign: 'center', margin: '50px 10px'}}>
-            <h3>Please, log in to go to this page...</h3>
+            <h3 style={{marginBottom: '5vh'}}>Please, log in to go to this page...</h3>
+            <Link to='/login'>Go to log in here</Link>
         </div>
     );
 }
