9afd49067d83bcb3af7c1be5fe7c1a612193f97d
daniel <daniel.palmdal@gmail.com>
2021-06-07 15:51:08 +0200

    Created booking context and added usercontext to booking context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,42 +7,48 @@ import HomePage from "./pages/HomePage";
 import AllMovies from "./pages/AllMovies";
 import AboutPage from "./pages/AboutPage";
 import UserPage from "./pages/UserPage";
-import UserContextProvider from './contexts/UserContext';
-import MovieContextProvider from './contexts/MovieContext';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MovieInfoPage from './pages/MovieInfoPage'
+import UserContextProvider from "./contexts/UserContext";
+import MovieContextProvider from "./contexts/MovieContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieInfoPage from "./pages/MovieInfoPage";
 import Footer from "./components/Footer";
+import BookingContextProvider from "./contexts/BookingContext";
 
 function App() {
   return (
     <div className="App">
       <MovieContextProvider>
         <UserContextProvider>
-          <BrowserRouter>
-            <Navbar />
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route exact path="/login">
-              <LoginPage />
-            </Route>
-            <Route exact path="/profile">
-              <UserPage />
-            </Route>
-            <Route exact path="/showing/:showingId" component={Showing} />
-            <Route exact path="/profile" />
-            <Route exact path="/about" component={AboutPage} />
-            <Route exact path="/register" component={SignUp} />
-            <Route exact path="/login" />
-            <Route exact path="/allmovies">
-              <AllMovies />
-            </Route>
-            <Route exact path="/movie-info/:movieId" component={MovieInfoPage}/>
-            <Footer />  
-          </BrowserRouter>
+          <BookingContextProvider>
+            <BrowserRouter>
+              <Navbar />
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/login">
+                <LoginPage />
+              </Route>
+              <Route exact path="/profile">
+                <UserPage />
+              </Route>
+              <Route exact path="/showing/:showingId" component={Showing} />
+              <Route exact path="/profile" />
+              <Route exact path="/about" component={AboutPage} />
+              <Route exact path="/register" component={SignUp} />
+              <Route exact path="/login" />
+              <Route exact path="/allmovies">
+                <AllMovies />
+              </Route>
+              <Route
+                exact
+                path="/movie-info/:movieId"
+                component={MovieInfoPage}
+              />
+              <Footer />
+            </BrowserRouter>
+          </BookingContextProvider>
         </UserContextProvider>
       </MovieContextProvider>
-      
     </div>
   );
 }
---
+++ frontend/src/contexts/BookingContext.js
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+import { UserContext } from "./UserContext";
+
+export const BookingContext = createContext();
+
+
+const BookingContextProvider = (props) => {
+    const { activeUser} = useContext(UserContext)
+    
+    console.log('in booking context', activeUser);
+  const values = {};
+
+  return (
+    <BookingContext.Provider value={values}>
+      {props.children}
+    </BookingContext.Provider>
+  );
+};
+export default BookingContextProvider;
