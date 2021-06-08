732d690741552123ed5fb546a549f3aa5b9c85b5
Kyd <info@kydkit.com>
2021-06-08 10:20:25 +0200

    delete function in backend

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -6,5 +6,6 @@ const bookingController = require("../controllers/bookingController");
 
 router.post('', bookingController.createBooking)
 router.get('/:bookingid', bookingController.getBookingById)
+router.delete("/:bookingid", bookingController.deleteBooking)
 
 module.exports = router;
