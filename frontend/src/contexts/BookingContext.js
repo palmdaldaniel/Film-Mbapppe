9375010d4334364bde9a146edaa5b17043cac09e
Kyd <info@kydkit.com>
2021-06-08 11:51:10 +0200

    added movie context

--- frontend/src/contexts/BookingContext.js
+++ frontend/src/contexts/BookingContext.js
@@ -1,12 +1,15 @@
 import { createContext, useContext } from "react";
 import { UserContext } from "./UserContext";
+import { MovieContext } from "./MovieContext"; 
 
 export const BookingContext = createContext();
 
 
 const BookingContextProvider = (props) => {
     const { activeUser} = useContext(UserContext)
+    const { showing } = useContext(MovieContext); 
     
+    console.log('in booking context...showing', showing);
     console.log('in booking context', activeUser);
   const values = {};
 
