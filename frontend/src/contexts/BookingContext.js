140ac34029347b76e056613c6f6ae3b2b1b960ef
Kyd <info@kydkit.com>
2021-06-08 13:15:42 +0200

    added some stuff

--- frontend/src/contexts/BookingContext.js
+++ frontend/src/contexts/BookingContext.js
@@ -1,17 +1,23 @@
 import { createContext, useContext } from "react";
 import { UserContext } from "./UserContext";
-import { MovieContext } from "./MovieContext"; 
+import { MovieContext } from "./MovieContext";
 
 export const BookingContext = createContext();
 
 
 const BookingContextProvider = (props) => {
-    const { activeUser} = useContext(UserContext)
-    const { showing } = useContext(MovieContext); 
-    
-    console.log('in booking context...showing', showing);
-    console.log('in booking context', activeUser);
-  const values = {};
+  const { activeUser } = useContext(UserContext)
+  const { showing } = useContext(MovieContext);
+
+  console.log('in booking context...showing', showing);
+  console.log('in booking context', activeUser);
+
+  const createBooking = () => {
+  }
+
+  const values = {
+    createBooking
+  };
 
   return (
     <BookingContext.Provider value={values}>
