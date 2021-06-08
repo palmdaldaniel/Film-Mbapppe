02a8d8dc3ec6b39846fa902e97d09563a98b475d
Blerta <blertatoska30@gmail.com>
2021-06-08 13:17:40 +0200

    added deleteBooking in bookingContext

--- frontend/src/contexts/BookingContext.js
+++ frontend/src/contexts/BookingContext.js
@@ -1,19 +1,50 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext , useState} from "react";
 import { UserContext } from "./UserContext";
 
 export const BookingContext = createContext();
 
 
 const BookingContextProvider = (props) => {
-    const { activeUser} = useContext(UserContext)
+    const { activeUser} = useContext(UserContext);
+    const [bookingId, setBookingId] = useState([]);
+    //const [userBookings, setUserBookings] = useState([]);
     
-    console.log('in booking context', activeUser);
-  const values = {};
+    //console.log('in booking context', activeUser);
+    //const values = {};
+
+
+
+// delete Booking 
+const deleteBooking = async (bookingId, userId) => {
+  await fetch(`/api/v1/users/${bookingId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+
+    //getBooking (activeUser._id);
+    //getBooking(userId);
+
+  };
+
+ 
+
+ const values = {
+   deleteBooking,
+   activeUser,
+   bookingId,
+   setBookingId
+  //getBooking,
+  //userBooking,
+  //setUserBookings
+
+};
 
   return (
-    <BookingContext.Provider value={values}>
-      {props.children}
-    </BookingContext.Provider>
-  );
+    <BookingContext.Provider value={values}>{props.children}</BookingContext.Provider>
+  ); 
 };
+
 export default BookingContextProvider;
+
