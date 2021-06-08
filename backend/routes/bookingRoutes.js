a70aacd80032297a0e67237851fc885e82423a56
Johannes <joh_hernehult@hotmail.com>
2021-06-08 09:24:46 +0200

    can get bookings by showing id

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const bookingController = require("../controllers/bookingController");
 
 
-router.post('', bookingController.createBooking)
+router.get('/:showingId', bookingController.getBookingsByShowingId)
 router.get('/:bookingid', bookingController.getBookingById)
+router.post('', bookingController.createBooking)
 
 module.exports = router;
