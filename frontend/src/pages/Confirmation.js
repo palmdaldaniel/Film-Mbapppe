e2ddc042587f7b793f227adc45d231057eb4d40a
Kyd <info@kydkit.com>
2021-06-09 22:16:10 +0200

    all data works on confirmation page

--- frontend/src/pages/Confirmation.js
+++ frontend/src/pages/Confirmation.js
@@ -1,11 +1,11 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom'
 import { BookingContext } from '../contexts/BookingContext';
 import s from "../css/Confirmation.module.css";
 
 const Confirmation = () => {
   const history = useHistory();
-  const { currentBooking, showing, totalPrice } = useContext(BookingContext);
+  const { currentBooking, showing } = useContext(BookingContext);
 
   useEffect(() => {
   }, [])
@@ -14,68 +14,84 @@ const Confirmation = () => {
     history.push("/");
   }
 
-  console.log("currentbooking", currentBooking);
-  console.log("showing", showing);
-
-  let ticketTypes;
-  let childTix = 0; 
-  let adultTix = 0; 
-  let srTix = 0; 
+  //get sum of each type of ticket
+  let adultTix = 0;
+  let childTix = 0;
+  let srTix = 0;
   if (currentBooking) {
-        {currentBooking.tickets.map((cbTicket, i) => {
-          if(cbTicket.type === "children"){
-            childTix += 1;
-            return
-          }
-          if(cbTicket.type === "adult"){
-            adultTix += 1; 
-            return
-          }
-          if(cbTicket.type === "senior"){
-            srTix += 1; 
-            return
-          }
-        })} 
+    currentBooking.tickets.forEach((cbTicket) => {
+      if (cbTicket.type === "adult") {
+        adultTix += 1;
+      }
+      if (cbTicket.type === "children") {
+        childTix += 1;
+      }
+      if (cbTicket.type === "senior") {
+        srTix += 1;
+      }
+    })
   }
 
-  console.log(childTix);
-  console.log(adultTix);
-  console.log(srTix);
+  //only prints ticket type those ticket type have been bought
+  let aTicketSum;
+  let cTicketSum;
+  let sTicketSum;
+  if (adultTix > 0) {
+    aTicketSum = (
+      <span>{adultTix} Adult</span>
+    )
+  }
+  if (childTix > 0) {
+    cTicketSum = (
+      <span>{childTix} Child</span>
+    )
+  }
+  if (srTix > 0) {
+    sTicketSum = (
+      <span>{srTix} Senior</span>
+    )
+  }
 
   let info = "";
   if (currentBooking) {
     info = (
       <>
         <div className={s.confirmationContainer}>
-
-          <strong>{showing.film.Title}</strong>
-          <p>{showing.film.Runtime}</p>
-          <p>{showing.saloon.name}</p>
-          <p>{showing.date}</p>
-          <p>{showing.time}</p>
-          <p>{`Booking No: ${(currentBooking._id).slice(18)}`}</p>
-          <p>Total Seats: {currentBooking.tickets.length} </p>
-
           <div>
+            <strong className={s.title}>{showing.film.Title}</strong>
+            <p>Duration: {showing.film.Runtime}</p>
+          </div>
+
+          <div className={s.locationTime}>
+            <span>{showing.saloon.name}</span>
+            <span>{showing.date}</span>
+            <span>{showing.time}</span>
+          </div>
+          <p>{`Booking No: ${(currentBooking._id).slice(18)}`}</p>
+
+          <div className={s.rowAndSeat}>
             {currentBooking.tickets.map((cbTicket, i) => (
               <div key={i}>
                 <span>{`Ticket: ${i + 1}, Row: ${cbTicket.rowNumber}, Seat: ${cbTicket.seatingNumber}`}</span>
               </div>
             ))}
           </div>
-          <div>
-            {currentBooking.tickets.map((cbTicket, i) => (
-              <div key={i}>
-                <span>{cbTicket.type}</span>
-              </div>
-            ))}
+
+          <div className={s.tixtypes}>
+            {aTicketSum}
+            {cTicketSum}
+            {sTicketSum}
           </div>
 
-          <div>
-            {ticketTypes}
+          <div className={s.totals}>
+            <p>{`Total Seats:
+            ${currentBooking.tickets.length}`}
+            </p>
+            <p>{`Total price: ${currentBooking.tickets.reduce((acc, cur) => {
+              return acc + cur.price
+            }, 0)}`}
+            </p>
           </div>
-
-          <p>Total price = {totalPrice}</p>
 
         </div>
       </>
@@ -83,11 +99,9 @@ const Confirmation = () => {
   }
   return (
     <div className={s.mainContainer}>
-
-      <h1>Booking Confirmed!</h1>
+      <h1 className={s.confirmMessage}>Booking Confirmed!</h1>
       {info}
-
-      <button className={s.button} onClick={handleClick}>Back To Homepage</button>
+      <button className={s.button} onClick={handleClick}>To Homepage</button>
     </div>
   );
 }
