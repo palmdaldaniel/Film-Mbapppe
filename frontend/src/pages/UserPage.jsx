d274149dbdce1ef94c01bfc9594b97a1c19617d9
Kyd <info@kydkit.com>
2021-06-11 14:46:52 +0200

    cleaned up files

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,11 +1,9 @@
-import { useContext, useState, useEffect } from 'react';
-import { UserContext } from '../contexts/UserContext';
-import { BookingContext } from '../contexts/BookingContext';
-import styles from '../css/userPage.module.css'
-import UserInfo from '../components/UserInfo';
-import BookingCard from '../components/BookingCard';
-
-
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { BookingContext } from "../contexts/BookingContext";
+import styles from "../css/userPage.module.css";
+import UserInfo from "../components/UserInfo";
+import BookingCard from "../components/BookingCard";
 
 const UserPage = () => {
   const { activeUser } = useContext(UserContext);
@@ -13,8 +11,6 @@ const UserPage = () => {
 
   //Used for booking card trashcan rendering
   const prev = useState(true);
-  console.log("upcoming movies", upcomingBookings);
-  console.log("previously seen", previousBookings);
 
   return (
     <div className={styles.container}>
@@ -25,31 +21,36 @@ const UserPage = () => {
             <div className={styles.upcoming}>
               <h2 className={styles.h2}>Upcoming movies</h2>
               <div className={styles.movieGrid}>
-                {upcomingBookings.length < 0
-                  ? (upcomingBookings.map((booking, i) => (<BookingCard
-                    booking={booking}
-                    prev={!prev}
-                    key={i} />)))
-                  : (<h5 className={styles.noMoviesMsg}>No upcoming bookings...</h5>)}
+                {upcomingBookings.length < 0 ? (
+                  upcomingBookings.map((booking, i) => (
+                    <BookingCard booking={booking} prev={!prev} key={i} />
+                  ))
+                ) : (
+                  <h5 className={styles.noMoviesMsg}>
+                    No upcoming bookings...
+                  </h5>
+                )}
               </div>
             </div>
             <div className={styles.previous}>
               <h2 className={styles.h2}>Previous movies</h2>
               <div className={styles.movieGrid}>
-                {previousBookings.length < 0
-                  ? (previousBookings.map((booking, i) => (<BookingCard
-                    booking={booking}
-                    prev={prev}
-                    key={i} />)))
-                  : (<h5 className={styles.noMoviesMsg}>No previous bookings...</h5>)}
+                {previousBookings.length < 0 ? (
+                  previousBookings.map((booking, i) => (
+                    <BookingCard booking={booking} prev={prev} key={i} />
+                  ))
+                ) : (
+                  <h5 className={styles.noMoviesMsg}>
+                    No previous bookings...
+                  </h5>
+                )}
               </div>
             </div>
           </div>
         </div>
       )}
-      
     </div>
   );
-}
+};
 
 export default UserPage;
