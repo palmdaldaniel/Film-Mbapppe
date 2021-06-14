3094181ae3a2a268f97687f8c6ee3bb864e863ae
Johannes <joh_hernehult@hotmail.com>
2021-06-14 10:44:28 +0200

    changed in usercontext regEx

--- frontend/src/contexts/UserContext.js
+++ frontend/src/contexts/UserContext.js
@@ -55,7 +55,7 @@ const UserContextProvider = (props) => {
         return;
       }
       if (!newPassword.match(RegExp(
-        "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{5,})"))) {
+        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,}$"))) {
           setMessage("Need a stronge password");
           setTimeout(() => {
             setMessage(null);
