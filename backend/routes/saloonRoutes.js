e5043591570ca00a8332eb3e5144eae354f09b67
Kyd <info@kydkit.com>
2021-06-15 14:48:45 +0200

    clean up console.log backend

--- backend/routes/saloonRoutes.js
+++ backend/routes/saloonRoutes.js
@@ -7,6 +7,6 @@ router.get("/:saloonId", saloonController.getSaloonById);
 router.put("/:saloonId", saloonController.editSaloon);
 router.delete("/:saloonId", saloonController.removeSaloon);
 router.post("", saloonController.addSaloon);
-router.get("", saloonController.getAllSaloons); 
+router.get("", saloonController.getAllSaloons);
 
 module.exports = router;
